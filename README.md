5e3cba2cf846a080f78d034724fffdde6e017916
Matias Beorlegui <mbeorlegui@frba.utn.edu.ar>
2021-04-15 21:20:43 -0300

    Arreglo error de coma

---
+++ README.md
@@ -0,0 +1,15 @@
+# 2021_MIXTO_GRUPO_1
+
+### Repositorio de TPs de la materia Sintaxis y Semántica de los Lenguajes - UTN FRBA
+
+### Profesor: Santiago Ferreiros
+
+### Integrantes del grupo:
+
+| Nombre        			| Mail           					| Curso		| Usuario  	|
+| ------------- 			|-------------						|--------	| -----		|
+| Matías Beorlegui      	| mbeorlegui@frba.utn.edu.ar 		|	K2002	| [@mbeorlegui](https://github.com/mbeorlegui) |
+| Olaf Querol				| olafquerol@frba.utn.edu.ar    	| 	K2001	| [@olafq](https://github.com/olafq)|
+| Lucas Rodriguez 			| lucasrodriguez@frba.utn.edu.ar	|   K2001	| [@lucasirod](https://github.com/lucasirod) |
+| Felipe Alborch			| falborchdeguzman@frba.utn.edu.ar  |   K2002	| [@felipealborch](https://github.com/FelipeAlborch) |
+| Limberth Vasquez Estrada 	| lvasquezestrada@frba.utn.edu.ar 	|   K2001	| [@limberth-estrada-utn](https://github.com/limberth-estrada-utn) |
---
+++ TP_1/TP1_con_archivos.c
@@ -0,0 +1,89 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+void RecorrerArchivo(FILE*, FILE*, bool*);
+int MaquinaDeEstados(char caracter, int estado);
+
+enum estados { q0,
+               q1,
+               q2,
+               q3,
+               q4,
+               q5,
+               ERROR };
+
+void main(void) {
+    FILE* aEntrada = fopen("entrada.txt", "r");
+    FILE* aSalida = fopen("salida.txt", "w");
+    bool finalDelArchivo = false;
+
+    while (finalDelArchivo == false) {
+        RecorrerArchivo(aEntrada, aSalida, &finalDelArchivo);
+    }
+
+    fclose(aEntrada);
+    fclose(aSalida);
+}
+
+void RecorrerArchivo(FILE* aEntrada, FILE* aSalida, bool* finalDelArchivo) {
+    if (aEntrada != NULL) {
+        char numeroArchivoEntrada;
+        int estado = q0;
+        int caracterActual = 0;
+
+        fread(&numeroArchivoEntrada, sizeof(char), 1, aEntrada);
+        while (!feof(aEntrada) && numeroArchivoEntrada != ',') {
+            estado = MaquinaDeEstados(numeroArchivoEntrada, estado);
+            printf("%c", numeroArchivoEntrada);
+            fwrite(&numeroArchivoEntrada, sizeof(char), 1, aSalida);
+            fread(&numeroArchivoEntrada, sizeof(char), 1, aEntrada);
+        }
+
+        if (feof(aEntrada) != 0) {
+            *finalDelArchivo = true;  // <------
+        }
+
+        if (estado == q2)
+            fprintf(aSalida, "\t\t->\t\tDECIMAL\n");
+        else if (estado == q4)
+            fprintf(aSalida, "\t\t->\t\tHEXADECIMAL\n");
+        else if (estado == q1 || estado == q5)
+            fprintf(aSalida, "\t\t->\t\tOCTAL\n");
+        else
+            fprintf(aSalida, "\t\t->\t\tNO RECONOCIDO\n");
+    }
+}
+
+int MaquinaDeEstados(char caracter, int estado) {
+    int q6 = ERROR;
+    int tabla[7][5] = {
+        {q1, q2, q2, q6, q6},
+        {q5, q5, q6, q6, q3},
+        {q2, q2, q2, q6, q6},
+        {q4, q4, q4, q4, q6},
+        {q4, q4, q4, q4, q6},
+        {q5, q5, q6, q6, q6},
+        {q6, q6, q6, q6, q6}};
+
+    switch (caracter) {
+        case '0':
+            estado = tabla[estado][0];
+            break;
+        case '1' ... '7':
+            estado = tabla[estado][1];
+            break;
+        case '8' ... '9':
+            estado = tabla[estado][2];
+            break;
+        case 'a' ... 'f':
+        case 'F' ... 'F':
+            estado = tabla[estado][3];
+            break;
+        case 'x':
+        case 'X':
+            estado = tabla[estado][4];
+            break;
+        default:
+            break;
+    }
+}
