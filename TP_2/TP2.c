ee1d65fadf752753c8b00db5c342ce7e71d0bade
FelipeAlborch <82057459+FelipeAlborch@users.noreply.github.com>
2021-06-06 23:17:11 -0300

    Agrego funcion para eliminar espacios de la cadena

--- TP_2/TP2.c
+++ TP_2/TP2.c
@@ -10,9 +10,25 @@ void main(void) {
     bool estadoFinalDeLaPila = true, estadoDeError = false;
     t_estado estadoPila = {q0, $};
     char operacion[30], cimaPila;
+    int a=0, i, largo;
 
     printf("Ingrese la operacion a analizar: ");
-    scanf("%29s", operacion);
+    gets(operacion);
+    
+    printf("Operacion (con espacios): ");
+    puts(operacion);
+    
+    largo=strlen(operacion);
+    
+    while(operacion[a]!='\0'){
+        while(operacion[a]==' '){
+            for(i=a;i<largo;i++){ 
+                operacion[i]=operacion[i+1];
+            }
+        largo--;
+        }
+        a++;  
+    } 
 
     recorrerCadena(estadoPila, &pila, operacion, &estadoDeError);
     verificarPilaVacia(&pila, &estadoFinalDeLaPila);
