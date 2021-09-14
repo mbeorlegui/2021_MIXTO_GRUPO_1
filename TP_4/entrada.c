6466bae5c7123cd4be525338c51ebf3980c96e04
Matias Beorlegui <mbeorlegui@frba.utn.edu.ar>
2021-09-14 12:39:13 -0300

    Agrego categorias lexicas y lectura de archivo de entrada

---
+++ TP_4/entrada.c
@@ -0,0 +1,22 @@
+
+int main() {
+    int a = 0;
+    int consOctal = 0256;
+    $ int conHexa = 0x78A;
+    int consDec = 11, conDec2 = 22;
+    char constChar = 'a', otrConst = 'b';
+    printf("LiteralCadena\n");
+    $ if (a = 0) {
+        a++;
+    }
+    else {
+        a--;
+    }
+    //ComentarioCorto
+
+    /* 
+        Comentario
+        Largo
+    */
+    return a;
+}
